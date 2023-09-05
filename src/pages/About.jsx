09b8bfa419e33cde22488ea2677269d0913bc1a6
Lucas Baronio <lbaronio20@hotmail.com>
2023-09-04 23:49:23 -0300

    feat: add github contribution

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="flex">
+      <span>About</span>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className=" h-[50vh]">
+        <img src="https://ssr-contributions-svg.vercel.app/_/lucasbaronio?chart=3dbar&gap=0.6&scale=2&flatten=1&animation=wave&animation_duration=4&animation_delay=0.06&animation_amplitude=24&animation_frequency=0.1&animation_wave_center=0_3&format=svg&weeks=30&theme=green&dark=true" />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
