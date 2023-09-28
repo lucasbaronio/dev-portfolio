5dcc2ff4b4285d0d9c20f24b3397fa82d2221c68
Lucas Baronio <lbaronio20@hotmail.com>
2023-09-28 16:00:34 -0300

    feat: add github calendar and nro contributions

---
+++ src/constants/images.js
@@ -0,0 +1,2 @@
+export const GITHUB_CALENDAR_IMG =
+  'https://ssr-contributions-svg.vercel.app/_/lbaronio?chart=3dbar&gap=0.6&scale=2&flatten=0&animation=wave&animation_duration=3&animation_delay=0.03&animation_amplitude=24&animation_frequency=0.1&animation_wave_center=19_3&format=svg&weeks=50&theme=native&dark=true';
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GITHUB_CALENDAR_IMG } from '../constants/images';
+
+const COLORS = ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353'];
+
+const Box = () => {
+  return (
+    <div className="flex items-center gap-1">
+      <span className=" mr-1">Less</span>
+      {COLORS.map((color) => (
+        <svg key={color} width="12" height="12">
+          <rect width="12" height="12" fill={color} rx="2" ry="2"></rect>
+        </svg>
+      ))}
+      <span className=" ml-1">More</span>
+    </div>
+  );
+};
 
 const Projects = () => {
+  const [githubContributions, setGithubContributions] = useState(
+    sessionStorage.getItem('githubContributions'),
+  );
+
+  useEffect(() => {
+    const fetchGithubData = async (username) => {
+      try {
+        const response = await fetch(
+          `https://github-contributions-api.jogruber.de/v4/${username}?y=last`,
+        );
+        const result = await response.json();
+        console.log(result);
+        setGithubContributions(result?.total?.lastYear);
+        window.sessionStorage.setItem('githubContributions', result?.total?.lastYear);
+      } catch (error) {
+        console.error(error);
+        return undefined;
+      }
+    };
+    if (!githubContributions) fetchGithubData('lbaronio');
+  }, []);
+
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className=" h-[50vh]">
-        <img src="https://ssr-contributions-svg.vercel.app/_/lbaronio?chart=3dbar&gap=0.6&scale=2&flatten=1&animation=wave&animation_duration=4&animation_delay=0.06&animation_amplitude=24&animation_frequency=0.1&animation_wave_center=0_3&format=svg&weeks=30&theme=green&dark=true" />
+      <div className=" relative w-full max-w-4xl mx-8 xl:mx-4">
+        <img src={GITHUB_CALENDAR_IMG} />
+        {githubContributions ? (
+          <span className=" absolute bottom-0 lg:bottom-[20%] left-0 w-1/2">
+            {githubContributions} contributions in the last year
+          </span>
+        ) : null}
+        <div className=" absolute top-0 sm:top-1/3 right-0">
+          <Box />
+        </div>
       </div>
     </div>
   );
