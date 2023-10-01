16f893b7e8c21e7dab33261744a9e22d7114b7c9
Lucas Baronio <lbaronio20@hotmail.com>
2023-10-01 18:25:33 -0300

    fix: responsive bugs

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -25,7 +25,10 @@ function RootLayout() {
       <main className=" grid grid-rows-[48px_auto_1fr_64px] sm:grid-rows-[48px_48px_1fr_64px] grid-cols-[1fr] md:grid-rows-[48px_48px_1fr_64px] md:grid-cols-[250px_1fr] lg:grid-rows-[48px_1fr_64px] lg:grid-cols-[250px_1fr] w-full h-full rounded-xl border border-slate-800 dark:bg-slate-900 bg-slate-900	dark:text-slate-400 text-slate-400">
         <div className="md:col-span-2 lg:col-span-1 md:border-r border-b border-slate-800">
           <div className="flex items-center justify-between h-12">
-            <span className="pl-4">Lucas Baronio</span>
+            <div className="flex items-center pl-2 gap-2">
+              <img src="/lb-logo-white.png" width="30" height="30" />
+              <span>Lucas Baronio</span>
+            </div>
             <div className=" block sm:hidden mx-3 cursor-pointer">
               {navOpened ? (
                 <RxCross1 size={30} onClick={() => setNavOpened((prevState) => !prevState)} />
--- src/pages/Hello.jsx
+++ src/pages/Hello.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import VerticalCarousel from '../components/VerticalCarousel';
+// import VerticalCarousel from '../components/VerticalCarousel';
 
 const Hello = () => {
   return (
     <div className="flex h-full w-full">
       <div className="flex flex-wrap w-full relative">
-        <div className=" flex basis-96 items-center justify-center flex-1">
+        <div className=" flex basis-96 items-center justify-center flex-1 px-4">
           <div className="flex flex-col my-5">
             <h4 className=" text-lg">Hi all. I am</h4>
             <h1 className=" animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-6xl font-thin my-2">
               Lucas Baronio
             </h1>
-            <pre className=" flex text-2xl text-blue-300">
+            <pre className=" flex text-lg sm:text-2xl text-blue-300">
               ${' '}
-              <h2 className=" animate-typing overflow-hidden border-r-2 border-r-orange-500 whitespace-nowrap pr-1">
+              <h2 className=" animate-typing overflow-hidden border-r-2 border-r-orange-500 pr-1">
                 Software Engineer Fullstack
               </h2>
             </pre>
           </div>
         </div>
-        <div className=" flex basis-96 flex-1 items-center justify-center h-full w-full">
+        <div className=" flex basis-96 flex-1 items-center justify-center h-full w-full px-4">
           <iframe
             className="w-[700px] md:w-[400px] lg:w-[700px] h-full lg:h-[700px]"
             src="https://snake-game-three-mu.vercel.app/"
