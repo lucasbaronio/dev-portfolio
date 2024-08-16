af4229d2cc3cf239803b017017a8da2934aab5c1
Lucas Baronio <lbaronio20@hotmail.com>
2024-08-16 18:59:48 -0300

    fix: vercel

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Root from './Root';
-import i18n from './providers/i18next/i18nextSSR.js';
-import { I18nextProvider } from 'react-i18next';
-import { BrowserRouter } from 'react-router-dom';
+// import Root from './Root';
+// import i18n from './providers/i18next/i18nextSSR.js';
+// import { I18nextProvider } from 'react-i18next';
+// import { BrowserRouter } from 'react-router-dom';
 // import { Router } from './router';
 
-const lng = document.documentElement.lang || 'es';
+// const lng = document.documentElement.lang || 'es';
 
-i18n.changeLanguage(lng);
+// i18n.changeLanguage(lng);
 
 ReactDOM.hydrateRoot(
   document.getElementById('root'),
   <React.StrictMode>
-    <Root>
+    {/* <Root>
       <I18nextProvider i18n={i18n}>
         <BrowserRouter>
-          {/* <Router /> */}
-          <div>Holaaa</div>
+          <Router />
         </BrowserRouter>
       </I18nextProvider>
-    </Root>
+    </Root> */}
+    <div>Holaaa</div>
   </React.StrictMode>,
 );
--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import Root from './Root';
-import { I18nextProvider } from 'react-i18next';
-import { StaticRouter } from 'react-router-dom/server';
+// import Root from './Root';
+// import { I18nextProvider } from 'react-i18next';
+// import { StaticRouter } from 'react-router-dom/server';
 // import { Router } from './router';
 
 export function render({ path, i18n }) {
   const html = ReactDOMServer.renderToString(
     <React.StrictMode>
-      <Root>
+      {/* <Root>
         <I18nextProvider i18n={i18n}>
           <StaticRouter location={path}>
-            {/* <Router /> */}
-            <div>Holaaa</div>
+            <Router />
           </StaticRouter>
         </I18nextProvider>
-      </Root>
+      </Root> */}
+      <div>Holaaa</div>
     </React.StrictMode>,
   );
   return { html };
