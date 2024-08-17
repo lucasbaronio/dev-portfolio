13034a623f2b159e5f833e9d7237bcd7a4a6a3cd
Lucas Baronio <lbaronio20@hotmail.com>
2024-08-17 14:50:20 -0300

    fix: vercel

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Root from './Root';
-// import i18n from './providers/i18next/i18nextSSR.js';
-// import { I18nextProvider } from 'react-i18next';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Router } from './router';
+import Root from './Root';
+import i18n from './providers/i18next/i18nextSSR.js';
+import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './router';
 
-// const lng = document.documentElement.lang || 'es';
+const lng = document.documentElement.lang || 'es';
 
-// i18n.changeLanguage(lng);
+i18n.changeLanguage(lng);
 
 ReactDOM.hydrateRoot(
   document.getElementById('root'),
   <React.StrictMode>
-    {/* <Root>
+    <Root>
       <I18nextProvider i18n={i18n}>
         <BrowserRouter>
           <Router />
         </BrowserRouter>
       </I18nextProvider>
-    </Root> */}
-    <div>Holaaa</div>
+    </Root>
   </React.StrictMode>,
 );
